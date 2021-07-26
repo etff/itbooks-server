5e9d44ca408973daa97030df1c9540824cb709d8
etff <wish2rich@gmail.com>
2021-07-26 11:05:14 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/example/itbooks/ItbooksApplication.java
@@ -0,0 +1,13 @@
+package com.example.itbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItbooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItbooksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/itbooks/ItbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItbooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
